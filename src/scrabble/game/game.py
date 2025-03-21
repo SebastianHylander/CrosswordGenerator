c64a5f5e116cd541d7dff9008b705db737cb748b
SebastianHylander <hylandersebastian@gmail.com>
2025-03-21 12:52:15 +0100

    added default board

--- src/scrabble/game/game.py
+++ src/scrabble/game/game.py
@@ -1,7 +1,7 @@
 class Game:
     def __init__(self, size, dictionary_path):
         self.size = size
-        self.board = [[] for _ in range(size)]
+        self.board = [['#' for _ in range(size)] for _ in range(size)]
         self.dictionary = self._read_dictionary(dictionary_path)
     
     def _read_dictionary(self, dictionary_path):
